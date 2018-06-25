70414dd58ab61c1c83c5d1912e824c7d11269916
Rita Bennett-Chew and Will Swanson <will@>
2018-06-25 15:12:16 -0700

    Add failing test 2

--- lib/queen_attack.rb
+++ lib/queen_attack.rb
@@ -1,3 +1,7 @@
 def queen_attack(queen_location, opponent_location)
+  if queen_location[0] == opponent_location[0] && queen_location[1] == opponent_location[1]
+    false
+  # elsif 
 
+  end
 end
--- spec/queen_attack_spec.rb
+++ spec/queen_attack_spec.rb
@@ -1,8 +1,11 @@
-require("rpsec")
-require("queen_attack")
+require('rspec')
+require('queen_attack')
 
 describe('#queen_attack') do
   it('validates that the two pieces are not in the same location') do
     expect(queen_attack([1,1], [1,1])).to(eq(false))
+  end
+  it('check that the queen can attack a piece vertical to her') do
+    expect(queen_attack([1,1],[5,1])).to(eq(true))
   end
 end
