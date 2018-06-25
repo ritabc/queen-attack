40dbe88a0a860109e2f65aadc92bfa97fd261363
Rita Bennett-Chew and Will Swanson <will@>
2018-06-25 16:01:55 -0700

    last commit

--- lib/queen_attack.rb
+++ lib/queen_attack.rb
@@ -1,7 +1,7 @@
 def queen_attack(queen, opponent)
   if queen[0] == opponent[0] && queen[1] == opponent[1]
     false
-  elsif queen[1] == opponent[1] || queen[0] == opponent[0]
+  elsif queen[0] == opponent[0] || queen[1] == opponent[1]
     true
   elsif (queen[0] - opponent[0]).abs == (queen[1] - opponent[1]).abs
     true
